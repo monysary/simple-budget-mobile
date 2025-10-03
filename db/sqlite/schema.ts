0258fbe4d3cb063ecd5e3f8a1e5d8addd3dc931f
monysary <sary.mony@gmail.com>
2025-10-03 16:35:38 -0700

    defined rest of the tables

--- db/sqlite/schema.ts
+++ db/sqlite/schema.ts
@@ -4,9 +4,9 @@ import { sql } from 'drizzle-orm';
 
 const base_table = {
   id: text('id', {length: 36}).primaryKey().$default(() => Crypto.randomUUID()),
-  created_at: integer('created_at', { mode: 'timestamp'}).notNull().default(sql`CURRENT_TIMESTAMP`),
-  updated_at: integer('created_at', { mode: 'timestamp'}).notNull().default(sql`CURRENT_TIMESTAMP`),
-  deleted_at: integer('created_at', { mode: 'timestamp'}).notNull().default(sql`CURRENT_TIMESTAMP`)
+  created_at: text('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
+  updated_at: text('updated_at'),
+  deleted_at: text('deleted_at')
 };
 
 export const spending_tracker = sqliteTable('spending_tracker', {
@@ -22,4 +22,36 @@ export const user_profile = sqliteTable('user_profile', {
   first_name: text('first_name').notNull(),
   last_name: text('last_name').notNull(),
   monthly_income: real('monthly_income').notNull()
+});
+
+export const spending_period = sqliteTable('spending_period', {
+  ...base_table,
+  start_date: text('start_date').notNull(),
+  end_date: text('end_date').notNull(),
+  description: text('description'),
+  spending_tracker_id: text('spending_tracker_id').notNull().references(() => spending_tracker.id)
+});
+
+export const recurring_charge = sqliteTable('recurring_charge', {
+  ...base_table,
+  name: text('name').notNull(),
+  amount: integer('amount').notNull(),
+  description: text('description'),
+  type: text('type').notNull().references(() => recurring_charge_type.id)
+});
+
+export const recurring_charge_type = sqliteTable('recurring_charge_type', {
+  ...base_table,
+  system_name: text('system_name').notNull(),
+  display_name: text('display_name').notNull(),
+  order: integer('order')
+});
+
+export const transaction = sqliteTable('transaction', {
+  ...base_table,
+  name: text('name').notNull(),
+  amount: integer('amount').notNull(),
+  description: text('description'),
+  date: text('date').notNull(),
+  spending_period_id: text('spending_period_id').notNull().references(() => spending_period.id)
 });
